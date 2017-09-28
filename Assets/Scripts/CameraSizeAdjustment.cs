db4aa0d2bd2cc2eb2a9676a005baf381537d949a
Ilya Malinovsky <malinovsky239@yandex.ru>
2017-09-28 13:35:55 +0300

    Make game resolution independent

---
+++ Assets/Scripts/CameraSizeAdjustment.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class CameraSizeAdjustment : MonoBehaviour
+    {
+        private const float WidthInUnits = 21.6f;
+        private const float HeightInUnits = 10;
+        private const float TargetAspectRatio = WidthInUnits / HeightInUnits;
+
+        private void Awake()
+        {
+            Resize();
+        }
+
+        private void Resize()
+        {
+            var aspectRatio = (float)Screen.width / Screen.height;
+            if (aspectRatio < TargetAspectRatio)
+            {
+                var correctCameraHeight = TargetAspectRatio / aspectRatio * HeightInUnits;
+                GetComponent<Camera>().orthographicSize = correctCameraHeight / 2f;
+            }
+        }
+    }
+}
